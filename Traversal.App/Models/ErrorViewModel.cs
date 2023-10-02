fe7aa4e87bdf346c292f044ce8c03c0cd69e8950
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-10-02 15:49:05 +0300

    Add project files.

---
+++ Traversal.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Traversal.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
